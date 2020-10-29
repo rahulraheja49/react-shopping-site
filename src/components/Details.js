5ca4003deda5df632b637efc8608ef69679ff1c8
rahu49 <rraheja49@gmail.com>
2020-10-29 13:21:59 +0530

    first commit

---
+++ src/components/Details.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Details extends Component {
+  state = {};
+  render() {
+    return (
+      <div>
+        <h3>Hello from details</h3>
+      </div>
+    );
+  }
+}
+
+export default Details;
